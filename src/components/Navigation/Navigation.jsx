1dd37628b0343c67032bd6a3d4c9c891d2d18469
kaonashiiiiiii <priora102003@gmail.com>
2023-11-06 15:22:15 +0200

    fix

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 import { Menu } from 'antd'
 import { NavLink, useLocation } from 'react-router-dom'
 import styles from './navigation.module.css'
